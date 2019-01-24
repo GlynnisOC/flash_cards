129ecfdbc6d3d483bf5add86250e8bed7d5f1395
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-01-23 20:56:34 -0700

    Created turns class

---
+++ lib/turns.rb
@@ -0,0 +1,14 @@
+class Turn
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+# string that rep a guess
+  def guess
+
+  end
+# represents card being shown
+  def card
+
+  end
+end
