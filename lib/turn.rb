9dec31cfcfcc5e810c546c41f0472e012643914d
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-01-27 16:52:37 -0700

    Updates to card and turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,15 +2,16 @@ class Turn
   # what is within the parentheses is feeding the @value
   # @value can be named whatever, as long as the = value
   # matches the value within the parentheses
-attr_reader :guess, :card
+attr_reader :guess, :card, :category
 
-  def initialize(guess, card)
+  def initialize(guess, card, category)
     @guess = guess
     @card = card
+    @category = category
   end
 
   def correct?
-    @guess == card.answer
+    guess == Card.answer
   end
 # feedback is calling on the correct?
 # method defined above and will return
